90c88b951f0371513af4d090b4f793413adae8f8
mic050r <103114387+mic050r@users.noreply.github.com>
2023-07-17 00:44:13 +0900

    [2차시] int()함수와 float() 함수 조합하기

---
+++ Chapter02/int_float01.py
@@ -0,0 +1,7 @@
+## int() 함수 : 문자열을 int 자료형으로 반환합니다. int는 정수형의 의미합니다.
+## float() 함수 : 문자열을 float 자료형으로 변환합니다. float는 실수형 또는 부동 소수점을 의미합니다.
+output_a = int("52")
+output_b = float("52.273")
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
