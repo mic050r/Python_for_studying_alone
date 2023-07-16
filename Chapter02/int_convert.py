ac2dc480cbf34d8cb4870ff0531910eb737aa439
mic050r <103114387+mic050r@users.noreply.github.com>
2023-07-17 00:36:49 +0900

    [2차시] int() 함수 활용하기

---
+++ Chapter02/int_convert.py
@@ -0,0 +1,9 @@
+## int() 함수 : 문자열을 int 자료형으로 반환합니다. int는 정수형의 의미
+string_a = input("입력A> ")
+int_a = int(string_a)
+
+string_b = input("입력B> ")
+int_b = int(string_b)
+
+print("문자열 자료:", string_a + string_b)
+print("숫자 자료:", int_a + int_b)
